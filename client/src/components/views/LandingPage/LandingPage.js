29f818755668742f353aa18b8c90c7fa97cf23e8
ohasis <dhgkdud4002@naver.com>
2021-09-02 02:02:57 +0900

    메인화면 등록된 비디오 출력

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,29 +1,64 @@
-import React, {useEffect} from 'react';
-import {Card, Icon, Avartar, Col, Typography, Row} from 'antd';
+import React, {useEffect, useState} from 'react';
+import {Card, Icon, Avatar, Col, Typography, Row} from 'antd';
+import Axios from 'axios';
+import moment from 'moment';
 
 const {Title} = Typography
 const {Meta} = Card
 
 function LandingPage() {
 
+    const [Videos, setVideo] = useState([])
 
-    // const renderCards = Videos.map((video, index) => {
+    useEffect(() => {
 
-    // })
+        Axios.get('/api/video/getVideos')
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data)
+                    setVideo(response.data.videos)
+                } else {
+                    alert('비디오 가져오기 실패')
+                }
+            })
+        
+    }, []) //한번만 불러오고 싶으면 []비워두기
+
+    const renderCards = Videos.map((video, index) => {
+
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor(video.duration - minutes * 60);
+
+        return <Col lg={6} md={8} xs={24}>
+            <a href={`/video/${video._id}`} >
+                <div style={{position:'relative'}}>
+                <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
+                    <div className=" duration">
+                        <span>{minutes} : {seconds}</span>
+                    </div>
+                </div>
+            </a>
+            <br />
+            <Meta 
+                avatar={ //동그라미 user이미지
+                    <Avatar src={video.writer.image} />
+                }
+                title={video.title}
+                description="" />
+            <span>{video.writer.name} </span><br />
+            <span style={{ marginLeft: '3rem' }}> {video.views}</span> - 
+            <span> {moment(video.createdAt).format("MMM Do YY")} </span>
+        </Col>
+    })
+
     return (
         <div style={{width:'85%', margin:'3rem auto'}}>
             <Title level={2}>Recommended</Title>
             <hr />
             <Row gutter={[32,16]}>
-                <Col lg={6} md={8} xs={24}>
-                    <div style={{position:'relative'}}>
-                        <div className=" duration">
 
-                        </div>
-                    </div>
-                    <br />
-                    <Meta description="" />
-                </Col>
+                {renderCards}
+                
             </Row>
 
 
